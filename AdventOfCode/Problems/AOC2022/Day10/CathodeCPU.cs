2d0f5c1937dac134822bf19b603c7b748d893532
Amatsugu <khamraj@kaisei.app>
2022-12-10 14:23:32 -0500

    Day 10

---
+++ AdventOfCode/Problems/AOC2022/Day10/CathodeCPU.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode.Problems.AOC2022.Day10;
+internal class CathodeCPU
+{
+	public enum Instruction
+	{
+		NoOp,
+		AddX
+	}
+
+	public int X { get; private set; } = 1;
+	private int _cycleNumber = 1;
+	private int _programCounter;
+	private int _pending = -1;
+
+	public int[] ExecuteCode((Instruction ins, int value)[] code, int[] outputCycles, Func<int, int, int>? processor = null)
+	{
+		var result = new int[outputCycles.Length];
+
+		var ridx = 0;
+
+		if (processor == null)
+			processor = (c, x) => c * x;
+
+		ExecuteCode(code, (c, x) =>
+		{
+			if (ridx < outputCycles.Length && c == outputCycles[ridx])
+			{
+				result[ridx] = processor(c, x);
+				ridx++;
+			}
+		});
+
+		return result;
+	}
+	public void ExecuteCode((Instruction ins, int value)[] code, Action<int, int> processor)
+	{
+
+		while (_programCounter < code.Length)
+		{
+			var (ins, value) = code[_programCounter];
+
+			processor(_cycleNumber, X);
+
+			switch ((ins, _pending))
+			{
+				case { ins: Instruction.NoOp }:
+					_programCounter++;
+					break;
+				case { ins: Instruction.AddX, _pending : -1 }:
+					_pending = 1;
+					break;
+				case { ins: Instruction.AddX, _pending: 0 }:
+					X += value;
+					_programCounter++;
+					break;
+			}
+
+			
+
+			if(_pending >= 0)
+				_pending--;
+			_cycleNumber++;	
+		}
+
+	}
+
+
+}
---
+++ AdventOfCode/Problems/AOC2022/Day10/CathodeRayTube.cs
@@ -0,0 +1,56 @@
+﻿using AdventOfCode.Runner.Attributes;
+
+namespace AdventOfCode.Problems.AOC2022.Day10;
+
+[ProblemInfo(2022, 10, "Cathode-Ray Tube")]
+internal class CathodeRayTube : Problem<int, string>
+{
+	private (CathodeCPU.Instruction ins, int value)[] _code = Array.Empty<(CathodeCPU.Instruction ins, int value)>();
+
+	public override void CalculatePart1()
+	{
+		var cpu = new CathodeCPU();
+		var result = cpu.ExecuteCode(_code, new[] { 20, 60, 100, 140, 180, 220 });
+		Part1 = result.Sum();
+	}
+
+	public override void CalculatePart2()
+	{
+		var output = Enumerable.Repeat(' ', 6 * 40).ToArray();
+
+		var cpu = new CathodeCPU();
+		cpu.ExecuteCode(_code, (cycle, signal) =>
+		{
+			cycle -= 1;
+			if (cycle > output.Length)
+				return;
+
+
+			var pos = signal % 40;
+			var head = (cycle % 40);
+			var sprite = Math.Abs(pos - head);
+			if(sprite <= 1)
+				output[cycle] = '█';
+		});
+
+		var lines = output.Chunk(40).Select(r => new string(r));
+		Part2 = $"\n{string.Join("\n", lines)}";
+	}
+
+	public override void LoadInput()
+	{
+		var lines = ReadInputLines("input.txt");
+		_code = new (CathodeCPU.Instruction ins, int value)[lines.Length];
+		for (int i = 0; i < lines.Length; i++)
+		{
+			var ln = lines[i];
+			if (ln == "noop")
+				_code[i] = (CathodeCPU.Instruction.NoOp, 0);
+			else
+			{
+				var instruction = ln.Split(' ');
+				_code[i] = (Enum.Parse<CathodeCPU.Instruction>(instruction[0], true), int.Parse(instruction[1]));
+			}
+		}
+	}
+}
