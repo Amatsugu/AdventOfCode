c1196e8dc4a7bea8126e03878342ba9fb5dc3633
Amatsugu <khamraj@kaisei.app>
2022-12-12 23:10:17 -0500

    Day 11 part 1

---
+++ AdventOfCode/Problems/AOC2022/Day11/Monkey.cs
@@ -0,0 +1,73 @@
+﻿using System.Numerics;
+
+namespace AdventOfCode.Problems.AOC2022.Day11;
+
+internal class Monkey
+{
+	public int MonkeyNumber { get; set; }
+	public int InspectionCount { get; private set; }
+	public List<BigInteger> Items { get; set; }
+
+	private MonkeyOperator _operator;
+	private uint _operand;
+	private uint _divisor;
+	private int _trueTarget;
+	private int _falseTarget;
+
+	public Monkey(string[] lines)
+	{
+		Items = new List<BigInteger>();
+		MonkeyNumber = int.Parse(lines[0].Split(' ')[^1][0..^1]);
+		Items = lines[1].Split(": ")[^1].Split(", ").Select(v => BigInteger.Parse(v)).ToList();
+		var operation = lines[2].Split("= old ")[^1];
+		_operator = operation[0] switch
+		{
+			'*' => MonkeyOperator.Multiply,
+			'+' => MonkeyOperator.Add,
+			_ => MonkeyOperator.Add
+		};
+		if (!uint.TryParse(operation[1..], out _operand))
+			_operator = MonkeyOperator.Power;
+
+		_divisor = uint.Parse(lines[3].Split(' ')[^1]);
+		_trueTarget = int.Parse(lines[4].Split(' ')[^1]);
+		_falseTarget = int.Parse(lines[5].Split(' ')[^1]);
+	}
+
+	public BigInteger Inspect(BigInteger value, uint worryOffset = 3)
+	{
+		InspectionCount++;
+		value = Operate(value);
+		if(worryOffset != 0)
+			value /= worryOffset;
+		return value;
+	}
+
+	public int GetThrowTarget(BigInteger value)
+	{
+		return value % _divisor == 0 ? _trueTarget : _falseTarget;
+	}
+
+	private BigInteger Operate(BigInteger value)
+	{
+		return _operator switch
+		{
+			MonkeyOperator.Multiply => value * _operand,
+			MonkeyOperator.Add => value + _operand,
+			MonkeyOperator.Power => value * value,
+			_ => value
+		};
+	}
+
+	private enum MonkeyOperator
+	{
+		Multiply,
+		Add,
+		Power
+	}
+
+	public override string ToString()
+	{
+		return $"{MonkeyNumber}: ({InspectionCount}) [{string.Join(", ",Items)}]";
+	}
+}
---
+++ AdventOfCode/Problems/AOC2022/Day11/MonkeyInTheMiddle.cs
@@ -0,0 +1,67 @@
+﻿using AdventOfCode.Runner.Attributes;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode.Problems.AOC2022.Day11;
+[ProblemInfo(2022, 11, "Monkey in the Middle")]
+internal class MonkeyInTheMiddle : Problem<int, int>
+{
+	private Monkey[] _monkeysPart1 = Array.Empty<Monkey>();
+	private Monkey[] _monkeysPart2 = Array.Empty<Monkey>();
+
+	public override void CalculatePart1()
+	{
+		Simulate(_monkeysPart1, 20);
+		Part1 = _monkeysPart1.OrderByDescending(m => m.InspectionCount)
+			.Take(2)
+			.Select(m => m.InspectionCount)
+			.Aggregate((a, b) => a * b);
+	}
+
+	public override void CalculatePart2()
+	{
+		Simulate(_monkeysPart2, 10000, 0);
+		Part2 = _monkeysPart2.OrderByDescending(m => m.InspectionCount)
+			.Take(2)
+			.Select(m => m.InspectionCount)
+			.Aggregate((a, b) => a * b);
+	}
+
+	public override void LoadInput()
+	{
+		var lines = ReadInputLines("test.txt").Chunk(7);
+		_monkeysPart1 = lines.Select(ln => new Monkey(ln)).ToArray();
+		_monkeysPart2 = lines.Select(ln => new Monkey(ln)).ToArray();
+
+	}
+
+	private static void Simulate(Monkey[] monkeys, int rounds, uint worry = 3)
+	{
+		for (int i = 0; i < rounds; i++)
+		{
+			SimulateRound(monkeys, worry);
+		}
+	}
+
+	private static void SimulateRound(Monkey[] monkeys, uint worry = 3)
+	{
+		foreach (var monkey in monkeys)
+			SimulateTurn(monkey, monkeys, worry);
+	}
+
+
+	private static void SimulateTurn(Monkey monkey, Monkey[] monkeys, uint worry = 3) {
+		for (int i = 0; i < monkey.Items.Count; i++)
+		{
+			var item = monkey.Inspect(monkey.Items[i], worry);
+			var target = monkey.GetThrowTarget(item);
+			monkeys[target].Items.Add(item);
+		}
+		monkey.Items.Clear();
+	}
+}
