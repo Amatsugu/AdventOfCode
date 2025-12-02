625a9e5ad506c17d4d1bcc501cc83a370360d75f
Amatsugu <khamraj@kaisei.app>
2025-12-01 21:19:43 -0500

    Aoc 2025 day 1

---
+++ AdventOfCode/Problems/AOC2025/Day1/SecretEntrance.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using ZLinq;
+
+namespace AdventOfCode.Problems.AOC2025.Day1;
+
+[ProblemInfo(2025, 1, "Secret Entrance")]
+internal class SecretEntrance : Problem<int, int>
+{
+	public int[] Input { get; set; } = [];
+	public override void CalculatePart1()
+	{
+		var c = 0;
+		var v = 50;
+		foreach (var item in Input)
+		{
+			v += item;
+			v = v.Mod(100);
+			if (v == 0)
+				c++;
+		}
+		Part1 = c;
+	}
+
+	public override void CalculatePart2()
+	{
+		var c = 0;
+		var v = 50;
+		foreach (var item in Input)
+		{
+			var sign = int.Sign(item);
+			for (int i = 0; i < Math.Abs(item); i++)
+			{
+				v += sign;
+				v = v.Mod(100);
+				if (v == 0)
+					c++;
+			}
+		}
+		Part2 = c;
+	}
+
+	public override void LoadInput()
+	{
+		Input = ReadInputLines("input.txt")
+			.AsValueEnumerable()
+			.Select(l =>
+			{
+				return l[0] switch
+				{
+					'L' => -int.Parse(l[1..]),
+					'R' => int.Parse(l[1..]),
+					_ => throw new NotImplementedException()
+				};
+			}).ToArray();
+	}
+}
--- AdventOfCode/Utils/Extensions.cs
+++ AdventOfCode/Utils/Extensions.cs
@@ -1,16 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Numerics;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace AdventOfCode.Utils;
 public static class Extensions
 {
-
-
 	public static string AsJoinedString<T>(this IEnumerable<T> data, string delim = ", ")
 	{
 		return string.Join(delim, data);
+	}
+
+	public static T Mod<T>(this T value, T divisor) where T : INumber<T>
+	{
+		T remainder = value % divisor;
+
+		if (remainder < T.Zero)
+			return remainder + divisor;
+		else
+			return remainder;
 	}
 }
