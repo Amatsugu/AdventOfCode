7056c1516e544031e1e50bcc2be2c80465823a8b
Amatsugu <khamraj@kaisei.app>
2025-12-13 14:11:04 -0500

    day 7 setup

---
+++ AdventOfCode/Problems/AOC2025/Day7/Laboratories.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AdventOfCode.Problems.AOC2025.Day7;
+[ProblemInfo(2025, 7, "Laboratories")]
+internal class Laboratories : Problem<long, long>
+{
+	public override void CalculatePart1()
+	{
+		throw new NotImplementedException();
+	}
+
+	public override void CalculatePart2()
+	{
+		throw new NotImplementedException();
+	}
+
+	public override void LoadInput()
+	{
+		throw new NotImplementedException();
+	}
+}
